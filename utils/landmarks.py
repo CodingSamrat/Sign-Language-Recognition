105b1cad9eaafb415debf940506aaba64d78336f
Sam <samratbiswas986@gmail.com>
2023-05-22 17:39:11 +0530

    Updated 5 fingers colours + palm

--- utils/landmarks.py
+++ utils/landmarks.py
@@ -4,11 +4,14 @@ import cv2 as cv
 def draw_landmarks(image, landmark_point):
     
     #: Listing colours
-    neon_green = (20, 255, 57)
     magenta = (255, 0, 255)
     cyan = (255, 255, 0)
     yellow = (0, 255, 255)
-    blue = (255, 0, 0)
+    neon_green = (20, 255, 57)
+    neon_purple = (253, 18, 171)
+    neon_blue = (255, 81, 31)
+    neon_orange = (31, 91, 255)
+    neon_red = (49, 49, 255)
     white = (255, 255, 255)
     black = (0, 0, 0)
 
@@ -19,7 +22,7 @@ def draw_landmarks(image, landmark_point):
     if len(landmark_point) > 0:
 
         #: Thumb [3, 4]
-        skeletal_color = blue
+        skeletal_color = neon_green
         cv.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]), outline_color, 6)
         cv.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[3]), tuple(landmark_point[4]), outline_color, 6)
@@ -27,7 +30,7 @@ def draw_landmarks(image, landmark_point):
 
 
         #: Index finger [6, 7, 8]
-        skeletal_color = neon_green
+        skeletal_color = neon_purple
         cv.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]), outline_color, 6)
         cv.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[6]), tuple(landmark_point[7]), outline_color, 6)
@@ -37,7 +40,7 @@ def draw_landmarks(image, landmark_point):
 
 
         #: Middle finger [10, 11, 12]
-        skeletal_color = cyan
+        skeletal_color = neon_red
         cv.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]), outline_color, 6)
         cv.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[10]), tuple(landmark_point[11]), outline_color, 6)
@@ -47,7 +50,7 @@ def draw_landmarks(image, landmark_point):
 
 
         #: Ring finger [14, 15, 16]
-        skeletal_color = yellow
+        skeletal_color = neon_blue
         cv.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]), outline_color, 6)
         cv.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[14]), tuple(landmark_point[15]), outline_color, 6)
@@ -57,19 +60,17 @@ def draw_landmarks(image, landmark_point):
 
 
         #: Little finger [18, 19, 20]
-        skeletal_color = magenta
+        skeletal_color = neon_orange
         cv.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]), outline_color, 6)
         cv.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]), outline_color, 6)
         cv.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]), outline_color, 6)
         cv.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]), skeletal_color, 2)
-
-        #:Changed to default Skeletal colour
-        skeletal_color = white
         
         
         #: Palm [1, 5, 9, 13, 17, 0]
+        skeletal_color = yellow
         cv.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]), outline_color, 6)
         cv.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[1]), tuple(landmark_point[2]), outline_color, 6)
@@ -84,6 +85,9 @@ def draw_landmarks(image, landmark_point):
         cv.line(image, tuple(landmark_point[13]), tuple(landmark_point[17]), skeletal_color, 2)
         cv.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]), outline_color, 6)
         cv.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]), skeletal_color, 2)
+        
+        #:Changed to default Skeletal colour
+        skeletal_color = white
 
     #: Key Points
     for index, landmark in enumerate(landmark_point):
